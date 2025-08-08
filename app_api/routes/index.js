da374c96fc429e77e516e401da4d979c5b953df2
Shawn Millin <shawn.millin@snhu.edu>
2025-08-07 20:07:08 -0500

    Module 6 completed base line

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -2,12 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 const tripsController = require('../controllers/trips');
+
+//define route for trips endpoint
 router
    .route('/trips')
-   .get(tripsController.tripsList); 
+   .get(tripsController.tripsList) //GET method routes triplist
+   .post(tripsController.tripsAddTrip); //POST method adds a Trip
    
 router
    .route('/trips/:tripCode')
-   .get(tripsController.tripsFindByCode);
+   .get(tripsController.tripsFindByCode)
+   .put(tripsController.tripsUpdateTrip);
+   
 
 module.exports = router;
